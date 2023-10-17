f1951c67b906605269bbecbd838fc50dbad239e4
atabekdemurtaza <mychannel21war@gmail.com>
2023-10-17 01:56:39 +0100

    CBV is fixed

---
+++ movies/api/cbv_views.py
@@ -0,0 +1,53 @@
+from rest_framework.views import APIView
+from movies.models import Movie
+from .serializers import MovieSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class MovieList(APIView):
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MovieDetail(APIView):
+
+    def get(self, request, lookup):
+        if lookup.isdigit():
+            movie = get_object_or_404(klass=Movie, id=lookup)
+        else:
+            movie = get_object_or_404(klass=Movie, name=lookup)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, lookup):
+        if lookup.isdigit():
+            movie = get_object_or_404(klass=Movie, id=lookup)
+        else:
+            movie = get_object_or_404(klass=Movie, name=lookup)
+
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, lookup):
+        if lookup.isdigit():
+            movie = get_object_or_404(klass=Movie, id=lookup)
+        else:
+            movie = get_object_or_404(klass=Movie, name=lookup)
+
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- movies/api/urls.py
+++ movies/api/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
-from .fbv_views import movie_list, movie_detail
+# from .fbv_views import movie_list, movie_detail
+from .cbv_views import MovieList, MovieDetail
 
+
+"""# Function-based-views
 urlpatterns = [
     path(route='list/', view=movie_list, name='movie-list'),
     path(route='list/<lookup>/', view=movie_detail, name='movie-detail')
+]"""
+
+# Class-based-views
+urlpatterns = [
+    path(
+        route='list/',
+        view=MovieList.as_view(),
+        name='movie-list'),
+    path(
+        route='list/<lookup>/',
+        view=MovieDetail.as_view(),
+        name='movie-detail')
 ]
