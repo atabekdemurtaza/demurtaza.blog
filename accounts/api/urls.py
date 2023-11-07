a52202d702253b6013da40c136ce9154b456e394
atabekdemurtaza <mychannel21war@gmail.com>
2023-11-07 00:43:30 +0000

    Commit new

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,10 +1,21 @@
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
-from .views import registration_view, logout_view
+# from .views import registration_view, logout_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
-    path(route='login/', view=obtain_auth_token, name='login'),
-    path(route='logout/', view=logout_view, name='logout'),
-    path(route='register/', view=registration_view, name='register'),
+    # path(route='login/', view=obtain_auth_token, name='login'),
+    # path(route='logout/', view=logout_view, name='logout'),
+    path(
+        'api/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'),
+    path('api/token/refresh/',
+         TokenRefreshView.as_view(),
+         name='token_refresh'),
+    # path(route='register/', view=registration_view, name='register'),
 ]
